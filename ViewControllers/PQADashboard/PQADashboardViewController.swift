c0ea24ebbd29c4e13988a59bcab23a292993f96a
DEV-MinhNN <nguyenngocminh16@gmail.com>
2016-01-12 16:05:52 +0700

    -Change

--- ViewControllers/PQADashboard/PQADashboardViewController.swift
+++ ViewControllers/PQADashboard/PQADashboardViewController.swift
@@ -12,8 +12,6 @@ class PQADashboardViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
         self .createTabBarView()
     }
 
@@ -52,25 +50,35 @@ class PQADashboardViewController: UITabBarController {
 
         let pqaCenterView = PQAHomeViewController() as PQAHomeViewController
         pqaCenterView.view.backgroundColor = UIColor .greenColor();
-
+        
         let pqaCenterNav: UINavigationController = UINavigationController.init(rootViewController: pqaCenterView)
         pqaCenterNav.navigationBarHidden = false
+        let iconCenter = UITabBarItem(title: "", image: UIImage(named: "IconTabBar")?.imageWithRenderingMode(UIImageRenderingMode.AlwaysOriginal), selectedImage: UIImage(named: "IconTabBar")?.imageWithRenderingMode(UIImageRenderingMode.AlwaysOriginal))
+        pqaCenterNav.tabBarItem = iconCenter
         pqaCenterNav.tabBarItem.tag = 2
-        
+
         let pqaHomeNav: UINavigationController = UINavigationController.init(rootViewController: pqaHomeVC)
         pqaHomeNav.navigationBarHidden = false
+        let iconHome = UITabBarItem(title: "Home", image: UIImage(named: "IconTabBarHome")?.imageWithRenderingMode(UIImageRenderingMode.AlwaysOriginal), selectedImage: UIImage(named: "IconTabBarHome-ac")?.imageWithRenderingMode(UIImageRenderingMode.AlwaysOriginal))
+        pqaHomeNav.tabBarItem = iconHome
         pqaHomeNav.tabBarItem.tag = 0
 
         let pqaSearchNav: UINavigationController = UINavigationController.init(rootViewController: pqaSearchVC)
         pqaSearchNav.navigationBarHidden = false
+        let iconSearch = UITabBarItem(title: "Search", image: UIImage(named: "IconTabBarS")?.imageWithRenderingMode(UIImageRenderingMode.AlwaysOriginal), selectedImage: UIImage(named: "IconTabBarS-ac")?.imageWithRenderingMode(UIImageRenderingMode.AlwaysOriginal))
+        pqaSearchNav.tabBarItem = iconSearch
         pqaSearchNav.tabBarItem.tag = 1
         
         let pqaInformationNav: UINavigationController = UINavigationController.init(rootViewController: pqaInformationVC)
         pqaInformationNav.navigationBarHidden = false
+        let iconInfo = UITabBarItem(title: "Information", image: UIImage(named: "IconTabBarInfo")?.imageWithRenderingMode(UIImageRenderingMode.AlwaysOriginal), selectedImage: UIImage(named: "IconTabBarInfo-ac")?.imageWithRenderingMode(UIImageRenderingMode.AlwaysOriginal))
+        pqaInformationNav.tabBarItem = iconInfo
         pqaInformationNav.tabBarItem.tag = 3
         
         let pqaMyPageNav: UINavigationController = UINavigationController.init(rootViewController: pqaMyPageVC)
         pqaMyPageNav.navigationBarHidden = false
+        let iconMypage = UITabBarItem(title: "My Page", image: UIImage(named: "IconTabBarMP")?.imageWithRenderingMode(UIImageRenderingMode.AlwaysOriginal), selectedImage: UIImage(named: "IconTabBarMP-ac")?.imageWithRenderingMode(UIImageRenderingMode.AlwaysOriginal))
+        pqaMyPageNav.tabBarItem = iconMypage
         pqaMyPageNav.tabBarItem.tag = 4
         
         self.viewControllers = [pqaHomeNav, pqaSearchNav, pqaCenterNav, pqaInformationNav, pqaMyPageNav]
